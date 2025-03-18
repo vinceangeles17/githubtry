bd4a2bede2480c493e567f4cd44739b559f4de4a
Vince Angeles <nerocarranza@gmail.com>
2025-03-18 10:44:47 +0800

    Update Angeles.php

--- Angeles.php
+++ Angeles.php
@@ -1,1 +1,12 @@
-Hello world
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Try</title>
+</head>
+<body>
+    <div>
+        <h2>Hello WORLD</h2>
+    </div>
+</body>
+</html>
